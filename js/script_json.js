8e45201701a1efc14c6802f4df390a695123a876
Rajesh Patra <p7.rajesh@gmail.com>
2015-10-22 00:43:45 +0530

    javascript file to show the json in ajax

---
+++ js/script_json.js
@@ -0,0 +1,49 @@
+(function(){
+
+	var link = document.getElementsByTagName("a")[0];
+
+	link.onclick = function(){
+
+		var xhr = new XMLHttpRequest();
+
+		xhr.onreadystatechange = function(){
+			if((xhr.readyState ==4) && xhr.status == 200 || xhr.status == 304){
+				var body = document.getElementsByTagName("body")[0];
+				
+				var json = JSON.parse(xhr.responseText);
+
+				var heading = json.heading;
+				var h2 = document.createElement("h2");
+				var h2Text = document.createTextNode(heading);
+				h2.appendChild(h2Text);
+
+				var list = document.createElement("ul");
+
+				var items = json.items
+
+				for (key in items) {
+					var item = items[key];
+					var li = document.createElement("li");
+					var liText = document.createTextNode(item);
+					li.appendChild(liText);
+					list.appendChild(li);
+
+				}
+
+				body.appendChild(h2);
+				body.appendChild(list);
+
+				body.removeChild(link);
+			}
+		};
+
+		xhr.open("GET", "files/ajax.json", true);
+
+		xhr.send(null);
+
+		return false;
+
+
+	};
+
+})();
